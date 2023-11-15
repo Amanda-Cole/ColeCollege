dd6537a9f5d7eb2680bd08353bb5ee80ac460b52
Amanda Cole <col22031@byui.edu>
2023-11-14 20:50:03 -0700

    test

--- ColeCollege/Program.cs
+++ ColeCollege/Program.cs
@@ -21,6 +21,7 @@ namespace ColeCollege
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
