aa9c3d69b6ea76fcb167d328ce2ce1da602aabc2
Amanda Cole <col22031@byui.edu>
2023-11-16 10:43:20 -0700

    Update related Data section

--- ColeCollege/Migrations/20231116161146_ComplexDataModel.Designer.cs
+++ ColeCollege/Migrations/20231116161146_ComplexDataModel.Designer.cs
@@ -4,14 +4,16 @@ using ColeCollege.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ColeCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231116161146_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ColeCollege/Migrations/20231116170252_TableData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColeCollege.Migrations
+{
+    public partial class TableData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
