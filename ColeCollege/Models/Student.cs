d9fe11b911b16e81a1d5b1b1015540efbaa1ad07
Amanda Cole <col22031@byui.edu>
2023-11-16 12:09:31 -0700

    final bits

--- ColeCollege/Models/Student.cs
+++ ColeCollege/Models/Student.cs
@@ -1,38 +1,18 @@
-﻿using System;
+﻿using ColeCollege.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ColeCollege.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        [Required]
-        public string LastName { get; set; }
-
-        [Display(Name = "First Name")]
-        [StringLength(50)]
-        [Required]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
-
-        [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
