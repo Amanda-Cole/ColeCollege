11e123119130da74d52b6cf5e4278a92e23f1550
Amanda Cole <col22031@byui.edu>
2023-11-16 12:15:59 -0700

    final database update

---
+++ ColeCollege/Migrations/20231116191252_extras.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColeCollege.Migrations
+{
+    public partial class extras : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
