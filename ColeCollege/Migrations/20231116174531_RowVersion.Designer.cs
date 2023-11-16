4c52f2634f85f457dec89b4313f7fc0e9119bcc0
Amanda Cole <col22031@byui.edu>
2023-11-16 11:46:33 -0700

    Handle conflicts

--- ColeCollege/Migrations/20231116174531_RowVersion.Designer.cs
+++ ColeCollege/Migrations/20231116174531_RowVersion.Designer.cs
@@ -4,14 +4,16 @@ using ColeCollege.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ColeCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231116174531_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72,11 +74,6 @@ namespace ColeCollege.Migrations
                     b.Property<string>("Name")
                         .HasMaxLength(50)
                         .HasColumnType("nvarchar(50)");
-
-                    b.Property<byte[]>("RowVersion")
-                        .IsConcurrencyToken()
-                        .ValueGeneratedOnAddOrUpdate()
-                        .HasColumnType("rowversion");
 
                     b.Property<DateTime>("StartDate")
                         .HasColumnType("datetime2");
--- ColeCollege/Models/Department.cs
+++ ColeCollege/Models/Department.cs
@@ -24,6 +24,8 @@ namespace ColeCollege.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
